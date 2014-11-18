f71d87c11ce78a82fbac69446ff3e41573f2bad1
Antonio Perez <antonio@hexperimental.com>
2014-11-18 00:34:50 -0800

    layout finished, almost all content

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php date_default_timezone_set('America/Los_Angeles');?>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="./wingfall.css">
+        <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+        <script src="./wingfall.js"></script>
+    </head>
+    <body>
+    <div id="logo"><img src="./img/logo.png" /></div>
+    <div id="cta"><a href="https://itunes.apple.com/us/app/wingfall/id858347376?ls=1&mt=8"><img src="./img/dlbadge.png" /></a></div>
+    <div id="stage">
+        <div id="dude"><img src="./img/dude.png" /></div>
+        <div id="section1" class="section">
+
+            <div class="description">Wingfall is a physics simulation game
+                 based around the aerodynamic drag force
+                 of free falling!</div>
+            <div class="caption">The further you fall, the higher your score.</div>
+
+        </div>
+        <div id="section2" class="section">
+            <div class="description">Tilt side to side to  achieve the perfect angle. </div>
+            <div class="caption">Based on real life aerodynamics</div>
+            <div class="sub">Balance your speed and don't lose control!</div>
+        </div>
+
+        <div id="section3" class="section">
+            <div class="sub">Time is on your side, but the sky is not.</div>
+
+            <div class="description">Crashing into a cloud means game over.
+                 The same goes for flying out of bounds.</div>
+         </div>
+        <div id="section4" class="section">
+           <div class="description"> lose to your friends in Game Center (because they don't have a life)</div>
+        </div>
+
+        <div id="section5" class="section"></div>
+    </div>
+    <div id="footer">
+        <img class="footer-logo" src="./img/hexLogo.png" />
+        <div>&copy; <?php print date('Y');?> Hexperimental Media Inc </div>
+    </div>
+    </body>
+</html>
+<!--
+Wingfall The further you fall, the higher your score. Wingfall is a physics simulation game based around the aerodynamic drag force of free falling!
+What is your freefalling strategy? Slow and steady or drop it like it’s hot? Tilt your freefaller side to side and back to front achieve the perfect angle. Balance your speed and don't lose control!
+Time is on your side, but the sky is not. Crashing into a cloud means game over. The same goes for flying out of bounds.
+Happy falling!
+Features:
+
+   * easy to play, hard to master
+   * based on real life aerodynamics
+   * record your stats and top score
+   * lose to your friends in Game Center (because they don't have a life)
+
+Keywords: freefall, fall, physics, simulation, sim, flying, sky, diving, skydive, down, parachute, jump, jumper, drop
+-->
---
+++ wingfall.js
@@ -0,0 +1,38 @@
+$( document ).ready(function() {
+    var lastScrollTop = 0;
+    var rotation =0;
+    window.onscroll = function (event) {
+
+        var $w = $(this),
+        scroll_top = $w.scrollTop(),
+        total_height = $("body").height(),
+        viewable_area = $w.height(),
+        scrollPercent = Math.floor((scroll_top + viewable_area) / total_height * 100);
+        var margin=50;
+        var posX = ( (scroll_top*100)/($("#stage").height()-150)+2);
+        var angleDelta = 1;
+        if(scroll_top< 0){return}
+        if(posX > 82){return}
+        if (scroll_top > lastScrollTop){
+            angleDelta=-5;
+        } else {
+            angleDelta=5;
+        }
+        rotation+= angleDelta;
+
+        if(rotation>45){rotation=45;}
+        if(rotation<-45){rotation=-45;}
+
+        $("#dude").rotate(rotation)
+        $("#dude").css('left',posX+'%')
+        lastScrollTop = scroll_top;
+    }
+
+});
+jQuery.fn.rotate = function(degrees) {
+    $(this).css({'-webkit-transform' : 'rotate('+ degrees +'deg)',
+                 '-moz-transform' : 'rotate('+ degrees +'deg)',
+                 '-ms-transform' : 'rotate('+ degrees +'deg)',
+                 'transform' : 'rotate('+ degrees +'deg)'});
+    return $(this);
+};
